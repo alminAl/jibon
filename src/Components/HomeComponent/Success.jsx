b28de986346e568ed5a0e52135093a7c2f3414a3
hossainmamun <hossainmamun337@gmailcom>
2022-09-19 00:31:44 +0600

    style update

--- src/Components/HomeComponent/Success.jsx
+++ src/Components/HomeComponent/Success.jsx
@@ -18,12 +18,21 @@ const Success = () => {
     speed: 500,
     autoplaySpeed: 4000,
     cssEase: "linear",
-    /* responsive: [
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -31,9 +40,9 @@ const Success = () => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
         },
       },
       {
@@ -43,7 +52,7 @@ const Success = () => {
           slidesToScroll: 1,
         },
       },
-    ], */
+    ],
   };
   return (
     <>
