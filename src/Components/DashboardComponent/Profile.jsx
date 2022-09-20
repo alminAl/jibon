8e30d9bfe4ea498deedf6a3dfd32f9c83f4bcc93
akash7766 <mraakash01932@gmail.com>
2022-09-21 01:41:31 +0600

    profile design complete

--- src/Components/DashboardComponent/Profile.jsx
+++ src/Components/DashboardComponent/Profile.jsx
@@ -1,9 +1,62 @@
 import React from "react";
+import "../DashboardComponent/profile.css";
 
 const Profile = () => {
   return (
-    <div>
-      <h1>This is Your Profile</h1>
+    <div className="py-5">
+      <div className="row g-4">
+        <div className="col-md-4">
+          <div className="px-3 ">
+            <div className="img-div">
+              <img
+                src="https://pbs.twimg.com/media/EYVxlOSXsAExOpX.jpg"
+                alt=""
+              />
+            </div>
+            <div className="mt-4 text-center">
+              <h6 className="text-success fw-bold">Mashrafi Bin Mortaza</h6>
+              <h6 className="text-primary fw-bolder">Admin</h6>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div className="mb-4">
+            <h2>Additional Details</h2>
+            <hr />
+          </div>
+          <div>
+            <table class="table bg-transparent">
+              <tbody>
+                <tr>
+                  <th scope="row">Present Address</th>
+                  <td>Tejgaon, Dhaka</td>
+                </tr>
+                <tr>
+                  <th scope="row">Permanent Address</th>
+                  <td>Jamalpur, Mymensing</td>
+                </tr>
+                <tr>
+                  <th scope="row">Email Address</th>
+                  <td>mash@gmail.com</td>
+                </tr>
+                <tr>
+                  <th scope="row">Contact Number</th>
+                  <td>+880 177854211</td>
+                </tr>
+                <tr>
+                  <th scope="row">Last Education Qualification</th>
+                  <td>Honers in Math, DU</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="text-center my-5">
+            <button className="btn mt-5 btn-outline-success">
+              Edit Profile
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
