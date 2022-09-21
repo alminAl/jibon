27996cfe3ed4485c0c92769206e7279db583a72d
Jamil Kawsher <jamil15-9734@diu.edu.bd>
2022-09-21 11:09:27 +0600

    event list added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
-import { Routes, Route } from "react-router-dom";
-import EventHome from "./Components/EeventLatest/EventHome.jsx";
-=======
 import { Routes, Route, useLocation } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 import AddEvent from "./Components/DashboardComponent/AddEvent.jsx";
 import EditProfile from "./Components/DashboardComponent/EditProfile.jsx";
 import OurEvents from "./Components/DashboardComponent/OurEvents.jsx";
 import Profile from "./Components/DashboardComponent/Profile.jsx";
->>>>>>> 6a9f0a54ec057accc18f0d7000d0ad9d2123bb66
 import Error from "./Components/Error/Error.jsx";
 import Home from "./Components/HomeComponent/Home.jsx";
 import Footer from "./Components/ReuseComponent/Footer.jsx";
 import Navigation from "./Components/ReuseComponent/Navigation.jsx";
-
+import EventHome from "./Components/EeventLatest/EventHome.jsx";
 function App() {
   const location = useLocation();
 
@@ -24,9 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route index element={<Home />} />
-        <Route path="/test" element={<EventHome />} />
         <Route path="*" element={<Error />} />
-
+        <Route path="/test" element={<EventHome />}></Route>
         <Route path="dashboard" element={<Dashboard />}>
           {/* dashboard routes */}
           <Route path="profile" element={<Profile />}></Route>
